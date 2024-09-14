1a4082fac3fbd385b9b498b932de61789a6304b4
Archit Agarwal <architagarwal147@gmail.com>
2024-09-14 09:14:11 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,30 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
+// Local fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+// Google fonts
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100","200","300","400","500","600","700","800","900"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={poppins.className}
       >
         {children}
       </body>
