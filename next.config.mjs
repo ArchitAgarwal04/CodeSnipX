dd9089845b9aac580a870b3c5c91877f9b3c55b1
Archit Agarwal <architagarwal147@gmail.com>
2024-09-15 20:10:01 +0530

    update9

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
+// Use CommonJS export
 export default nextConfig;
