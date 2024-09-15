0371744b306d7d7992598db48d8207b610bf2580
Archit Agarwal <architagarwal147@gmail.com>
2024-09-16 00:04:42 +0530

    update15

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -13,21 +13,34 @@ export const createOrUpdateUser = async (
   try {
     await connect();
 
-    const user = await User.findOneAndUpdate(
-      { clerkId: id },
-      {
-        $set: {
-          email: email_addresses[0].email,
+    // Check if the user exists first
+    let user = await User.findOne({ clerkId: id });
+
+    if (user) {
+      // If user exists, update their information
+      user = await User.findOneAndUpdate(
+        { clerkId: id },
+        {
+          $set: {
+            email: email_addresses[0].email,
+          },
         },
-      },
-      { new: true, upsert: true }
-    );
+        { new: true }
+      );
+    } else {
+      // If user doesn't exist, create a new user
+      user = new User({
+        clerkId: id,
+        email: email_addresses[0].email,
+      });
 
-    console.log(user)
+      await user.save(); // Save the new user to the database
+    }
 
+    console.log(user);
     return user;
+
   } catch (error) {
     console.log('Error creating or updating user:', error);
   }
 };
-
